25dc48a2a2be4db49e43b0470884453cf6fe785d
witelokk <witelokk@gmail.com>
2025-02-20 17:52:19 +0300

    intial project

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package com.witelokk
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
